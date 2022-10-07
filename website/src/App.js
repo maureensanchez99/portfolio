e952b3f76b3c0d35472ded0c6ea34ad891dec352
Maureen Sanchez <39846411+maureensanchez99@users.noreply.github.com>
2022-10-07 12:04:21 -0500

    setting up .js page

--- website/src/App.js
+++ website/src/App.js
@@ -3,21 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="nav-header">
+        <h2 className="title">Maureen Sanchez</h2>
+        <div className="nav-items">
+          <p className="home-btn">Home</p>
+          <p className="Projects">Projects</p>
+        </div>
+      </div>
     </div>
   );
 }
