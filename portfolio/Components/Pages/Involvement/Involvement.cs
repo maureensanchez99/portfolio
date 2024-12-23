3921d6e49438f63ad41d7825afb43200d0128996
Maureen Sanchez <39846411+maureensanchez99@users.noreply.github.com>
2024-12-22 18:21:31 -0800

    fixed class so involvement.cs can use its function properly

--- portfolio/Components/Pages/Involvement/Involvement.cs
+++ portfolio/Components/Pages/Involvement/Involvement.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Components;
 using System.Collections.Generic;
 
-namespace involvement.Components.Pages {
+namespace portfolio.Components.Pages {
     public class InvolvementBase : ComponentBase {
         public class InvolvementDetails {
             public string Event { get; set; } = string.Empty;
@@ -12,13 +12,15 @@ namespace involvement.Components.Pages {
             public string Type { get; set; } = string.Empty;
         }
 
-        protected List<InvolvementDetails> volunteer = new List<InvolvementDetails> {
-            new InvolvementDetails {
-                Event = "Hotel Operations Efficiency System",
-                Date = "Course",
-                Position = "A web application that serves as a platform to improve management efficiency for hotels.",
-                Description = "https://github.com/CSC-3380-Fall-2024/Team-7",
-                ImagePath = "",
+        protected List<InvolvementDetails> volunteerDetails = new List<InvolvementDetails>
+        {
+            new InvolvementDetails
+            {
+                Event = "Geaux Big 2024",
+                Date = "March 2024",
+                Position = "Volunteer",
+                Description = "",
+                ImagePath = "/images/volunteer/geauxbig.jpg",
                 Type = "LSU"
             }
         };
